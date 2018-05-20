2fecf8e4df5e84c9679ccaf87549b30332ec2a0d
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2018-05-19 22:01:52 -0700

    First working commit

---
+++ breakmywork.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QDesktopWidget, QLabel
+
+
+class BreakMyWork(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('breakmywork')
+        self.addLabel()
+        self.center()
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(
+                self,
+                'Message',
+                "Are you sure to quit?",
+                QMessageBox.Yes | QMessageBox.No,
+                QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def addLabel(self):
+        self.label1 = QLabel("Welcome to breakmywork!", self)
+        self.label1.setGeometry(0, 0, 250, 150)
+        self.label1.setAlignment(Qt.AlignCenter)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = BreakMyWork()
+    sys.exit(app.exec_())
